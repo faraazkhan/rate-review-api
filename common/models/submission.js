1e3e7a15febf54b4cff60cceed7fb26648f57d76
Faraaz Khan <faraaz@rationalizeit.us>
2014-11-09 16:54:59 -0600

    failed attempt at creating a custom method

--- common/models/submission.js
+++ common/models/submission.js
@@ -1,17 +1,36 @@
 module.exports = function(Submission) {
+  //Submission inherits from the PersistedModel base class
 
+  // Turn defaults off
   var isStatic = true;
+  var isNotStatic = false;
   var disabledNonStaticRemoteMethods = ['updateAttributes'];
   var disabledStaticRemoteMethods = ['create', 'upsert', 'exists', 'count',
-                               'update', 'find', 'findOne', 'findById',  'deleteById'];
-  disabledStaticRemoteMethods.forEach(function(method){
-    Submission.sharedClass.find(method, isStatic).shared = false; // this disables the remote method and deregisters it from Swagger
-  });
+    'update', 'find', 'findOne', 'findById',
+    'deleteById'];
+    //console.log(Submission.sharedClass);
+    disabledStaticRemoteMethods.forEach(function(method){
+      Submission.sharedClass.find(method, isStatic).shared = false; // this disables class level remote methods and deregisters it from Swagger
+    });
 
-  disabledNonStaticRemoteMethods.forEach(function(method){
-    Submission.sharedClass.find(method, false).shared = false;
-  });
+    disabledNonStaticRemoteMethods.forEach(function(method){
+      Submission.sharedClass.find(method, isNotStatic).shared = false; // this disables  object/instance level remote methods and deregisters them Swagger
+    });
 
+    // Custom Methods for our API
+    Submission.findSubmission = function(params, cb) {
+      cb(null, 'Attempting to create a custom method' + params);
+    }
+
+    Submission.remoteMethod(
+      'findSubmission',
+      {
+        http: {path: '/', verb: 'get'},
+        accepts: [{arg: 'state', type: 'string', required: true},
+                  {arg: 'issuerId', type: 'string'}],
+        returns: {arg: 'findSubmission', type: 'string'}
+      }
+    );
 };
 
 
